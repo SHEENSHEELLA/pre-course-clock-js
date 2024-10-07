483747778d220b2f4b76f27d336fd8f5c20dc851
Dzmitry Sashcheka <sheensheella@gmail.com>
2024-10-08 00:54:59 +0300

    Finish task #3

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-let timerId // переменная, которая будет хранить ID таймера
+let timerIds = [] // массив для хранения ID таймеров
 
 const startButton = document.getElementById('start')
 
-
 startButton.addEventListener('click', function () {
-  timerId = setInterval(updateClock, 1000) // запускаем  updateClock() каждую секунду
+  const timerId = setInterval(updateClock, 1000) // запускаем  updateClock() каждую секунду
+  timerIds.push(timerId) // сохраняем ID таймера в массив
 })
 
 const stopButton = document.getElementById('stop')
 stopButton.addEventListener('click', function () {
-  clearInterval(timerId) // останавливаем таймер
+  // останавливаем все таймеры
+  timerIds.forEach((id) => clearInterval(id))
+  timerIds = [] // очищаем массив таймеров
 })
 
 function updateClock() {
   const clock = document.getElementById('clock')
   const now = new Date()
-  const hours = now.getHours()
-  const minutes = now.getMinutes()
-  const seconds = now.getSeconds()
-  clock.textContent = hours + ':' + minutes + ':' + seconds
+  const hours = String(now.getHours()).padStart(2, '0') // добавляем 0 перед одиночными числами
+  const minutes = String(now.getMinutes()).padStart(2, '0')
+  const seconds = String(now.getSeconds()).padStart(2, '0')
+  clock.textContent = `${hours}:${minutes}:${seconds}` // выводим текущее время
 }
